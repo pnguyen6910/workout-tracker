24945cf935a3468246424041358686edbb8d76f0
Paul Nguyen <pnguyen6910@gmail.com>
2022-08-25 11:33:26 -0500

    ran props form horizontal scroll bar to home and finished logic for horizontal scrollbar

--- src/components/HorizontalScrollBar.js
+++ src/components/HorizontalScrollBar.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Box } from '@mui/material'
+import BodyPart from './BodyPart'
 
-const HorizontalScrollBar = () => {
+const HorizontalScrollBar = ({ data, bodyPart, setBodyParts }) => {
   return (
-    <div>HorizontalScrollBar</div>
+    <div>
+        {data.map((item) => (
+          <Box
+            key={item.id || item}
+            itemId={item.id || item}
+            title={item.id || item}
+            m="0 40px"
+          >
+            <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyParts}/>
+          </Box>
+        )
+      )}
+    </div>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,28 @@
-import React, { useState } from 'react'
-import { Box } from '@mui/material'
-import HeroBanner from '../components/HeroBanner'
-import SearchExercises from '../components/SearchExercises'
-import Exercises from '../components/Exercises'
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import HeroBanner from "../components/HeroBanner";
+import SearchExercises from "../components/SearchExercises";
+import Exercises from "../components/Exercises";
 
 const Home = () => {
-  return (
-        <Box>
-            <HeroBanner />
-            <SearchExercises />
-            <Exercises />
-        </Box>
-  )
-}
+  const [bodyPart, setBodyPart] = useState("all");
+  const [excercises, setExercises] = useState([]);
 
-export default Home
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises
+        setExcercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExcercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+    </Box>
+  );
+};
+
+export default Home;
